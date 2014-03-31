2dc020b72b68e60b3b6024dbdd105ff541bfc428
Alex <pilotchik@gmail.com>
2014-03-31 17:10:58 +0400

    0.1.0
    Готовый класс корабля

--- seabattle_oop.py
+++ seabattle_oop.py
@@ -7,21 +7,66 @@ class Ship():
     свойство (указывается при создании объекта):расположение (0 - горизонтальное, 1 - вертикальное)
     свойство (указывается при создании объекта):ключевая точка (тег в формате: "столбец_строка")
     свойство:массив со статусами точек, который формируется конструктором
+    свойство:массив с координатами точек корабля, который формируется конструктором
+    свойство:координаты точек вокруг корабля
     свойство:статус гибели корабля
     метод-конструктор:изменение массива со статусами точек, например [0,0,1,0]
-    метод:выстрел(координаты точек)
-    метод:вернуть координаты точек вокруг корабля
+    метод:выстрел(координаты точки), возвращает 1 - если попали, 2 - убил, 0 - мимо
     '''
     
-    #свойства
+    #свойства объектов, описанные в классе
     #массив со статусами точек корабля
     status_map = []
-
+    #массив с координатами точек корабля
+    coord_map = []
+    #точки вокруг корабля
+    around_map = []
+    #статус гибели корабля
     death = 0
     
     #метод-конструктор
     def __init__(self,length,rasp,keypoint):
-        print(length,rasp,keypoint)
+        #создать массивы status_map и coord_map (в зависимости от направления)
+        stolb = int(keypoint[0])
+        stroka = int(keypoint[2])
+        for i in range(length):
+            self.status_map.append(0)
+            #в зависимости от направления генерировать новые точки корабля
+            #0 - горизонт (увеличивать столбец), 1 - вертикаль (увеличивать строку)
+            if rasp == 0:
+                self.coord_map.append(str(stolb+i)+"_"+str(stroka))    
+            else:
+                self.coord_map.append(str(stolb)+"_"+str(stroka+i))
+        for point in self.coord_map:
+            ti = int(point[0])
+            tj = int(point[2])
+            for ri in range(ti-1,ti+2):
+                for rj in range(tj-1,tj+2):
+                    if ri>=0 and ri<=9 and rj>=0 and rj<=9:
+                        if not(str(ri)+"_"+str(rj) in self.around_map) and not(str(ri)+"_"+str(rj) in self.coord_map):
+                            self.around_map.append(str(ri)+"_"+str(rj))
 
-obj = Ship(4,0,"0_0")
-print(Ship.__doc__)
+    #выстрел
+    def shoot(self,shootpoint):
+        #определить номер точки и изменить её статус
+        status = 0
+        for point in range(len(self.coord_map)):
+            if self.coord_map[point] == shootpoint:
+                self.status_map[point] = 1
+                status = 1
+                break
+        if not(0 in self.status_map):
+            status = 2
+            self.death = 1
+        return status
+            
+
+obj = Ship(2,1,"0_1")
+print(obj.coord_map)
+print(obj.status_map)
+print(obj.around_map)
+print(obj.shoot("0_1"))
+print(obj.shoot("0_2"))
+
+flot = []
+flot.append(obj)
